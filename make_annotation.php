91d94db2cd5e6bada79113ea44e0d2bd716fe3f5
unknown <Kartikeya@.(none)>
2011-03-10 19:50:28 -0600

    create annotation in database

---
+++ make_annotation.php
@@ -0,0 +1,26 @@
+<?php
+  function add_annotation($l, $ct, $sn, $t, $a) {
+    $l  = addslashes($l);
+    $ct = addslashes($ct);
+    $sn = addslashes($sn);
+    $t  = addslashes($t);
+    $a  = addslashes($a);
+
+    mysql_connect("mysql.imwillow.com", "uiuc492", "492492!!") or die ("{status: 201, error:'". mysql_error() . "'}");
+    mysql_select_db("uiuc492");
+    
+    $query = "INSERT INTO Annotation (location, chart_type, series_name, time, annotation) VALUES ('" . $l . "','" . $ct . "','" . $sn . "','" . $t . "','" . $a . "');";
+    mysql_query($query) or die (mysql_error());
+    
+    return 1;
+  }
+  
+  $location     = $_GET["location"];
+  $chart_type  = $_GET["chart_type"];
+  $series_name  = $_GET["series_name"];
+  $time         = $_GET["time"];
+  $annotation   = $_GET["annotation"];
+  
+  add_annotation($location, $chart_type, $series_name, $time, $annotation);
+?>
+
