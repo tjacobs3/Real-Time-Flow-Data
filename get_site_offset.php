7adda2fe7cdf45bbbfe615db3fa2ad8fec29963a
unknown <Kartikeya@.(none)>
2011-03-18 15:06:31 -0600

    get default site offset

---
+++ get_site_offset.php
@@ -0,0 +1,27 @@
+<?php
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Content-type: application/json');
+
+$location = isset($_GET["location"]) ? $_GET["location"] : "U22";
+
+	$site_offsets = array(
+			"U22" => 663.29,
+			"D57" => 652.65,
+			"U84" => 600,
+			"D108" => 624.93
+		);
+
+	function site_name_to_offset()
+	{
+		global $site_offsets;
+		global $location;
+		if(array_key_exists($location, $site_offsets))
+		{
+			return $site_offsets[$location];
+		}
+	}
+
+$location_offset = site_name_to_offset();
+echo json_encode($location_offset);
+?>	
