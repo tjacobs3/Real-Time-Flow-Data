62f54a1cbd626c1174395d51521dd0b7a5e68b23
Chengyin Liu <chengyin.liu@gmail.com>
2011-04-06 16:43:36 -0500

    Setting db

---
+++ annotation_settings.php
@@ -0,0 +1,8 @@
+<?php
+$annotation_db_url  = "mysql.imwillow.com";   // MySQL database address
+$annotation_db_port = 3306;                   // MySQL port
+
+$annotation_db_user = "uiuc492";              // MySQL username
+$annotation_db_pass = "492492!!";             // MySQL password
+$annotation_db_db   = "uiuc492";              // MySQL database
+?>
